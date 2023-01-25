b1eca9181286e0bb223448c9de8f34999805a12d
Elmar Hinz <t3elmar@gmail.com>
2023-01-25 09:52:24 +0100

    adding the code

---
+++ card.js
@@ -0,0 +1,36 @@
+class HelloWorldCard extends HTMLElement {
+
+    config;
+    content;
+
+    setConfig(config) {
+        if (!config.entity) {
+            throw new Error('Please define an entity!');
+        }
+        this.config = config;
+    }
+
+    set hass(hass) {
+        const entityId = this.config.entity;
+        const state = hass.states[entityId];
+        const stateStr = state ? state.state : 'unavailable';
+
+        // done once
+        if (!this.content) {
+            // user makes sense here as every login gets it's own instance
+            this.innerHTML = `
+                <ha-card header="Hello ${hass.user.name}!">
+                    <div class="card-content"></div>
+                </ha-card>
+            `;
+            this.content = this.querySelector('div');
+        }
+        // done repeatedly
+        this.content.innerHTML = `
+            <p>The ${entityId} is ${stateStr}.</p>
+        `;
+    }
+
+}
+
+customElements.define('hello-world-card', HelloWorldCard);
